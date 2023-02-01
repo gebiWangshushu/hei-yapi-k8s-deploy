7a5f8fa14331b3531194862820288ebb710f9a8f
Heiner Wang <wangsiheng@siluzan.com>
2023-02-01 10:58:28 +0800

    修正yapi启动脚本

--- startup.sh
+++ startup.sh
@@ -3,13 +3,13 @@
 #初始化环境
 mkdir yapi && cd yapi
 
-#https://github.com/YMFE/yapi/archive/refs/heads/master.zip 下载原链接(github.91chi.fun是镜像)
-wget https://github.91chi.fun//https://github.com/YMFE/yapi/archive/refs/heads/master.zip
+wget https://github.com/YMFE/yapi/archive/refs/heads/master.zip #如果服务器访问不了github自己找个镜像
 unzip master.zip
 mv yapi-master vendors
 cp vendors/config_example.json ./config.json
 
 #复制完成后替换相关配置
+sed -i "2 a  \"closeRegister\":${CLOSE_REGISTER:-false}," config.json
 sed -i 's/\"adminAccount\": \"admin@admin.com\"/\"adminAccount\": '\"${YAPI_ADMIN_ACCOUNT}\"'/g' config.json
 sed -i 's/\"servername\": \"127.0.0.1\"/\"servername\": '\"${YAPI_DB_SERVERNAME}\"'/g' config.json
 sed -i 's/\"DATABASE\": \"yapi\"/\"DATABASE\": '\"${YAPI_DB_DATABASE}\"'/g' config.json
@@ -30,7 +30,9 @@ sed -i 's/\"port\": 465/\"port\": '${YAPI_MAIL_PORT:-465}'/g' config.json
 cd vendors
 rm package-lock.json #这个不删会导致安装失败
 npm install --production --registry https://registry.npm.taobao.org
-npm run install-server
+
+#初始化账号和数据库索引，可在第一次运行时单独跑(进入容器yapi/vendors目录运行)，不然后面每次重启都因为这句卡住： UnhandledPromiseRejectionWarning: Error: 初始化管理员账号 "***@gmail.com" 失败, E11000 duplicate key error collection: yapi.user index: email_1 dup key
+#npm run install-server 
 node server/app.js
 
 
